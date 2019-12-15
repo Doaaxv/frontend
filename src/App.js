0ad28f7b22b469767f166514a2cad8b994c5c2be
Doaa Alalawi <Doaa.alalawi@outlook.sa>
2019-12-15 15:19:14 +0300

    authentication - doaa a

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"
 import Landingpage from "./Components/Landingpage"
 import SideNav from "./Components/SideNav"
 import Portfolio from "./Components/Portfolio"
+import Login from "./Atho/Login"
+import Signup from "./Atho/Signup_emp"
 
 // import SideNav from './side-nav/SideNav'
-function App() {
-  return (
+export default class App extends Component {
+  render() {
+    return (
     <Router>
     
      <SideNav/>
-     
+
      <Route exact path="/" exact component={Landingpage}/>
      <Route exact path="/Portfolio" exact component={Portfolio}/>
+     <Route exact path="/login" exact component={Login}/>
+     <Route exact path="/Signup" exact component={Signup}/>
     </Router>
-  );
+    )
+  }
 }
-
-export default App;
