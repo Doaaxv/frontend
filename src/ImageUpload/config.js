6de0c42be2f66ac37b58b54323e0fa1179d70269
doaaxv <doaa.mh.uni@gmail.com>
2019-12-24 11:10:45 +0300

    final changes

--- src/ImageUpload/config.js
+++ src/ImageUpload/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 import 'firebase/storage'
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAzgjtvTRckZojic2wFEOwV2sMOTIgIT7o", //process.env.FIREBASE_API,
+    apiKey: process.env.FIREBASE_API,
     authDomain: "sei-project4.firebaseapp.com",
     databaseURL: "https://sei-project4.firebaseio.com",
     projectId: "sei-project4",
     storageBucket: "sei-project4.appspot.com",
-    messagingSenderId: "197890365238", //process.env.FIREBASE_MESSAGE_ID
+    messagingSenderId: process.env.FIREBASE_MESSAGE_ID,
     appId: "1:197890365238:web:96150e118fd4ffcdedc597",
     measurementId: "G-YK90CCT0SC"
 };
